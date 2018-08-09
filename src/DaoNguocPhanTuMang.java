699c0db4a30ac2268286ef15f0b2967fe2ce7e47
HaTien <havantien95tb@gmail.com>
2018-08-09 08:26:52 +0700

    lam tiep bai tap ve dao nguoc mang

--- src/DaoNguocPhanTuMang.java
+++ src/DaoNguocPhanTuMang.java
@@ -19,5 +19,18 @@ public class DaoNguocPhanTuMang {
             array[i] = sc.nextInt();
             i++;
         }
+        System.out.println(" cac phan tu trong mang");
+        for (int j = 0; j < array.length; j++ ) {
+            System.out.println(" array[j] "+ "\t");
+        }
+        for (int j = 0; j < array.length / 2; j++) {
+            int temp = array[j];
+            array[j] = array[size - 1 - j];
+            array[size - 1 - j] = temp;
+        }
+        System.out.printf( "dao nguoc mang: ", "");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
     }
 }
