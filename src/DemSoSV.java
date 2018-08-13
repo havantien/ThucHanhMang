fbc41b79fe731a89b883b0994e31ec0d773f41b7
HaTien <havantien95tb@gmail.com>
2018-08-14 01:19:07 +0700

    Chỉnh lại code

--- src/DemSoSV.java
+++ src/DemSoSV.java
@@ -6,28 +6,28 @@ public class DemSoSV {
         int[] array;
         Scanner sc = new Scanner(System.in);
         do {
-            System.out.println(" nhap size");
+            System.out.println(" nhập size ");
             size = sc.nextInt();
             if(size > 30)
-                System.out.println("size khong duoc qua 30");
+                System.out.println("size không được quá 30");
 
         } while (size > 30);
 
         array = new int[size];
         int i = 0;
         while ( i < array.length) {
-            System.out.println(" nhap diem cho sinh vien " + ( i + 1) + ":");
+            System.out.println(" nhập điểm cho sinh viên " + ( i + 1) + ":");
             array[i] = sc.nextInt();
             i++;
         }
         int count = 0;
-        System.out.println(" danh sach diem ");
+        System.out.println(" danh sách điểm ");
         for (int j = 0; j < array.length; j++) {
             System.out.println(array[j] + "\t");
             if (array[j] >= 5 && array[j] <= 10)
                 count++;
         }
-        System.out.println(" so luong sv vuot qua ki thi " + count);
+        System.out.println(" số lượng sinh viên qua kì thi " + count);
 
     }
 }
